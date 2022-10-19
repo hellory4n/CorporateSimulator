983bfee1b5c77fe3531fcd512386029a807a9794
hellory4n <hellory4n@gmail.com>
2022-10-19 15:44:44 -0500

    hi mom

---
+++ Scripts/Game.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class Game : Node2D {
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        GD.Print("hi mom");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
