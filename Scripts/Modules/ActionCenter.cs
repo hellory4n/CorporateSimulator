86726344196feac52beab93afec2f160d5ce5dc5
hellory4n <hellory4n@gmail.com>
2022-11-02 13:02:37 -0500

    do business things

---
+++ Scripts/Modules/ActionCenter.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class ActionCenter : TextureButton {
+    public override void _Ready() {
+        this.Connect("pressed", this, nameof(Click));
+    }
+
+    public void Click() {
+        if (GetNodeOrNull("/root/ActionCenter") == null) {
+            var yes = (PackedScene)ResourceLoader.Load("res://Scenes/ActionCenter.tscn");
+            Node2D OK = (Node2D)yes.Instance();
+            OK.ZIndex = 100;
+            GetTree().Root.AddChild(OK);
+            Global.PausedTime = true;
+        } else {
+            GetNode("/root/ActionCenter").QueueFree();
+            Global.PausedTime = false;
+        }
+    }
+}
