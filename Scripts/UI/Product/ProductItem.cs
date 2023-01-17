6d433032360074636ee7abfe256a87973a515be5
hellory4n <hellory4n@gmail.com>
2023-01-17 18:23:43 -0500

    just noticed the product list broke

--- Scripts/UI/Product/ProductItem.cs
+++ Scripts/UI/Product/ProductItem.cs
@@ -12,8 +12,8 @@ public class ProductItem : Control {
     }
 
     public override void _Ready() {
-        // sorry
         if (productProduct.Type == "rocket") {
+            // sorry
             SpaceProduct spaceProduct = JsonConvert.DeserializeObject<SpaceProduct>(objectProduct.ToString());
             GetNode<Label>("ProductName").Text = productProduct.Name;
             if (productProduct.MoneySpent > productProduct.MoneyGot)
@@ -32,6 +32,28 @@ public class ProductItem : Control {
                 "\nCarrying: " + spaceProduct.Carrying + "\nPeople: " + spaceProduct.People +
                 "\nDestination: " + spaceProduct.Destination + "\nSpeed: " + spaceProduct.Speed +
                 "%\nFuel: " + spaceProduct.Fuel + "\nSafety: " + spaceProduct.Safety + "%";
+        }
+        if (productProduct.Type == "book") {
+            // sorry
+            BookProduct bookProduct = JsonConvert.DeserializeObject<BookProduct>(objectProduct.ToString());
+            GetNode<Label>("ProductName").Text = productProduct.Name;
+            if (productProduct.MoneySpent > productProduct.MoneyGot)
+                GetNode<Label>("ProductInfo").Text = "Loss: $"+String.Format("{0:n0}",
+                    productProduct.MoneySpent-productProduct.MoneyGot);
+                else {
+                    GetNode<Label>("ProductInfo").Text = "Profit: $"+String.Format("{0:n0}",
+                        productProduct.MoneyGot-productProduct.MoneySpent);
+                }
+            
+            GetNode<Label>("ProductInfo").Text += "\nRevenue: $" + String.Format("{0:n0}",
+                productProduct.MoneyGot) + "\nSales: $" + String.Format("{0:n0}", productProduct.Sales) +
+                "\nCost: $" + String.Format("{0:n0}", productProduct.MoneySpent) + "\nRating: " +
+                productProduct.Rating + "/10\nRelease date: " + productProduct.ReleaseDate +
+                // just book stuff
+                "\nTheme: " + bookProduct.Theme + "\nGenre: " + bookProduct.Genre +
+                "Theme/Genre combination: " + bookProduct.ThemeGenreCombination + "/10\nLore: " +
+                bookProduct.Lore + "%\nStorytelling: " + bookProduct.Storytelling + "%\nCharacters: " +
+                bookProduct.Characters + "%";
         }
     }
 }
--- Scripts/UI/Product/ProductList.cs
+++ Scripts/UI/Product/ProductList.cs
@@ -6,8 +6,12 @@ public class ProductList : HBoxContainer {
     public override void _Ready() {
         var yes = (PackedScene)ResourceLoader.Load("res://Scenes/Product.tscn");
 
+        if (Global.Products.Count > 0)
+            GetNode<Label>("../../OhShoes").QueueFree();
+
         foreach (var product in Global.Products) {
             ProductItem OK = (ProductItem)yes.Instance();
+            GD.Print(product.ToString());
             // cast doesn't work haha yes
             OK.Init(JsonConvert.DeserializeObject<Product>(product.ToString()), product);
             AddChild(OK);
