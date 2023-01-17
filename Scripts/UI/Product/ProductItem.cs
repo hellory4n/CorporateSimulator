e7b35a4b9bccbe6570004d7dad598cda9b713506
hellory4n <hellory4n@gmail.com>
2023-01-17 17:11:36 -0500

    at least it doesn't crash anymore

--- Scripts/UI/Product/ProductItem.cs
+++ Scripts/UI/Product/ProductItem.cs
@@ -2,17 +2,14 @@ using Godot;
 using System;
 
 public class ProductItem : Control {
-    object product;
-    object productProduct;
+    Product productProduct;
 
-    public void Init(object product_) {
-        product = product_;
+    public void Init(Product product_) {
+        productProduct = product_;
     }
 
-p     public override void _Ready() {
-        Product productProduct = (Product)product;
-        if (productProduct.Type == "space") {
-            SpaceProduct spaceProduct = (SpaceProduct)productProduct;
+    public override void _Ready() {
+        if (productProduct is SpaceProduct spaceProduct) {
             GetNode<Label>("ProductName").Text = productProduct.Name;
             if (productProduct.MoneySpent > productProduct.MoneyGot)
                 GetNode<Label>("ProductInfo").Text = "Loss: $"+String.Format("{0:n0}",
@@ -21,17 +18,17 @@ p     public override void _Ready() {
                     GetNode<Label>("ProductInfo").Text = "Profit: $"+String.Format("{0:n0}",
                         productProduct.MoneyGot-productProduct.MoneySpent);
                 }
-
-                GetNode<Label>("ProductInfo").Text += "Revenue: $" + String.Format("{0:n0}",
-                    productProduct.MoneyGot) + "\nSales: $" + String.Format("{0:n0}", productProduct.Sales) +
-                    "\nCost: $" + String.Format("{0:n0}", productProduct.MoneySpent) + "Rating: " +
-                    productProduct.Rating + "/10\nRelease date: " + productProduct.ReleaseDate +
-                    // just space stuff
-                    "\nCarrying: " + spaceProduct.Carrying + "\nPeople: " + spaceProduct.People +
-                    "\nDestination: " + spaceProduct.Destination + "\nSpeed: " + spaceProduct.Speed +
-                    "%\nFuel: " + spaceProduct.Fuel + "\nSafety: " + spaceProduct.Safety + "%";
+            
+            GetNode<Label>("ProductInfo").Text = "Revenue: $" + String.Format("{0:n0}",
+                productProduct.MoneyGot) + "\nSales: $" + String.Format("{0:n0}", productProduct.Sales) +
+                "\nCost: $" + String.Format("{0:n0}", productProduct.MoneySpent) + "Rating: " +
+                productProduct.Rating + "/10\nRelease date: " + productProduct.ReleaseDate +
+                // just space stuff
+                "\nCarrying: " + spaceProduct.Carrying + "\nPeople: " + spaceProduct.People +
+                "\nDestination: " + spaceProduct.Destination + "\nSpeed: " + spaceProduct.Speed +
+                "%\nFuel: " + spaceProduct.Fuel + "\nSafety: " + spaceProduct.Safety + "%";
                 
-                GetNode<Label>("ProductName").SetGlobalPosition(new Vector2(RectPosition.x, 29));
+            GetNode<Label>("ProductName").SetGlobalPosition(new Vector2(RectPosition.x, 29));
         }
     }
 }
--- Scripts/UI/Product/ProductList.cs
+++ Scripts/UI/Product/ProductList.cs
@@ -7,14 +7,14 @@ public class ProductList : HBoxContainer {
 
         foreach (var product in Global.Products) {
             ProductItem OK = (ProductItem)yes.Instance();
-            OK.Init(product);
+            OK.Init(product as Product);
             AddChild(OK);
         }
         // there's a bug in godot where the last item is out of reach, this hack fixes it
         if (Global.Products.Count > 3) {
             object pain = Global.Products[Global.Products.Count-1];
             ProductItem OK = (ProductItem)yes.Instance();
-            OK.Init(pain);
+            OK.Init((Product)pain);
             AddChild(OK);
         }
         base._Ready();
