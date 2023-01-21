228f86349c583cd9da230f0750763972d6dda484
hellory4n <hellory4n@gmail.com>
2023-01-21 18:19:37 -0500

    i'm stupid

--- Scripts/UI/Default/DialogOk.cs
+++ Scripts/UI/Default/DialogOk.cs
@@ -7,6 +7,8 @@ public class DialogOk : TextureButton {
     }
 
     public void Click() {
+        if (GetNodeOrNull("/root/PauseThingy") != null)
+            GetNode("/root/PauseThingy").QueueFree();
         GetParent().QueueFree();
     }
 }
