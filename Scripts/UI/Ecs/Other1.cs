404d6d03f0473e224570607d5f2718d44cde1f09
hellory4n <hellory4n@gmail.com>
2023-02-09 15:03:43 -0500

    r

--- Scripts/UI/Ecs/Other1.cs
+++ Scripts/UI/Ecs/Other1.cs
@@ -1,21 +1,15 @@
 using Godot;
 using System;
 
-public class Other1 : TextureButton
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+public class Other1 : TextureButton {
+    public override void _Ready() {
+        this.Connect("pressed", this, nameof(Click));
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void Click() {
+        var yes = (PackedScene)ResourceLoader.Load("res://Scenes/News/Computer/EcsOther1.tscn");
+        Node2D OK = (Node2D)yes.Instance();
+        OK.ZIndex = 100;
+        GetTree().Root.AddChild(OK);
+    }
 }
