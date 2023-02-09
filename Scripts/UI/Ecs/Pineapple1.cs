c60cb494e174a431cec271f9f1dc152450d655ab
hellory4n <hellory4n@gmail.com>
2023-02-09 13:57:05 -0500

    hitoothbrush

---
+++ Scripts/UI/Ecs/Pineapple1.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class Pineapple1 : TextureButton
+{
+    public override void _Ready() {
+        this.Connect("pressed", this, nameof(Click));
+    }
+
+    public void Click() {
+        var yes = (PackedScene)ResourceLoader.Load("res://Scenes/News/Computer/EcsPineapple1.tscn");
+        Node2D OK = (Node2D)yes.Instance();
+        OK.ZIndex = 100;
+        GetTree().Root.AddChild(OK);
+    }
+}
