df7f85b6dcb220b34f0a46da9a4b405d38a10f70
hellory4n <hellory4n@gmail.com>
2023-02-13 14:21:09 -0500

    in tells 4

--- Scripts/UI/Ecs/InTells4.cs
+++ Scripts/UI/Ecs/InTells4.cs
@@ -2,19 +2,14 @@ using Godot;
 using System;
 
 public class InTells4 : TextureButton {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
+    public override void _Ready() {
+        this.Connect("pressed", this, nameof(Click));
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void Click() {
+        var yes = (PackedScene)ResourceLoader.Load("res://Scenes/News/Computer/EcsInTells4.tscn");
+        Node2D OK = (Node2D)yes.Instance();
+        OK.ZIndex = 100;
+        GetTree().Root.AddChild(OK);
+    }
 }
