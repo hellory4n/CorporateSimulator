803e1c3ddb6cbc1840b56c6d95dffd5b742e25b8
hellory4n <hellory4n@gmail.com>
2023-02-16 12:50:12 -0500

    g

---
+++ Scripts/UI/Ecs/And10.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class And10 : TextureButton {
+    public override void _Ready() {
+        this.Connect("pressed", this, nameof(Click));
+    }
+
+    public void Click() {
+        var yes = (PackedScene)ResourceLoader.Load("res://Scenes/News/Computer/EcsAnd10.tscn");
+        Node2D OK = (Node2D)yes.Instance();
+        OK.ZIndex = 100;
+        GetTree().Root.AddChild(OK);
+    }
+}
