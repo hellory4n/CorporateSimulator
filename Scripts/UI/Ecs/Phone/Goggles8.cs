74cc9b2aba00e252599c087699e5762abd92e367
hellory4n <hellory4n@gmail.com>
2023-03-04 22:06:22 -0500

    goggles 8

---
+++ Scripts/UI/Ecs/Phone/Goggles8.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public class Goggles8 : TextureButton {
+    public override void _Ready() {
+        this.Connect("pressed", this, nameof(Click));
+    }
+
+    public void Click() {
+        var yes = (PackedScene)ResourceLoader.Load("res://Scenes/News/Phone/EcsGoggles8.tscn");
+        Node2D OK = (Node2D)yes.Instance();
+        OK.ZIndex = 100;
+        GetTree().Root.AddChild(OK);
+    }
+}
