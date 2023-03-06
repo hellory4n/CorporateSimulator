b303cff834da91e49d69825dd46da92f678a25ce
hellory4n <hellory4n@gmail.com>
2023-03-06 14:26:23 -0500

    yes

--- Scripts/UI/NewProduct/Phones/PhoneSoc.cs
+++ Scripts/UI/NewProduct/Phones/PhoneSoc.cs
@@ -7,29 +7,81 @@ public class PhoneSoc : VBoxContainer {
         var yes = (PackedScene)ResourceLoader.Load("res://Scenes/NewProducts/PhoneSocButton.tscn");
         List<List<object>> cpus = new List<List<object>>();
         #region ohio 30 day trial
-        cpus.Add(new List<object>{"Napdragon", "8 Gen 2", "10", "380"});
-        cpus.Add(new List<object>{"MovieTek", "Dimension 9200", "10", "365"});
-        cpus.Add(new List<object>{"Napdragon", "888", "9", "350"});
-        cpus.Add(new List<object>{"MovieTek", "Dimension 8200", "9", "335"});
-        cpus.Add(new List<object>{"ByeSilicon", "Killin 9000", "8", "320"});
-        cpus.Add(new List<object>{"MovieTek", "Dimension 8000", "8", "305"});
-        cpus.Add(new List<object>{"Napdragon", "870", "8", "290"});
-        cpus.Add(new List<object>{"Napdragon", "865", "7", "275"});
-        cpus.Add(new List<object>{"MovieTek", "Dimension 1100", "7", "260"});
-        cpus.Add(new List<object>{"ByeSilicon", "Killin 990 5G", "7", "245"});
-        cpus.Add(new List<object>{"Napdragon", "778G Plus", "6", "230"});
-        cpus.Add(new List<object>{"ByeSilicon", "990 4G", "6", "215"});
-        cpus.Add(new List<object>{"MovieTek", "Dimension 920", "6", "200"});
-        cpus.Add(new List<object>{"Napdragon", "845", "5", "185"});
-        cpus.Add(new List<object>{"MovieTek", "Dimension 810", "5", "170"});
-        cpus.Add(new List<object>{"ByeSilicon", "Killin 810", "5", "155"});
-        cpus.Add(new List<object>{"MovieTek", "Helium G96", "4", "140"});
-        cpus.Add(new List<object>{"Napdragon", "835", "4", "125"});
-        cpus.Add(new List<object>{"ByeSilicon", "Killin 970", "4", "110"});
-        cpus.Add(new List<object>{"Multisoc", "Cheetah C618", "4", "95"});
-        cpus.Add(new List<object>{"Multisoc", "Cheetah C616", "3", "80"});
-        cpus.Add(new List<object>{"Napdragon", "662", "3", "65"});
-        cpus.Add(new List<object>{"MovieTek", "Helium G70", "3", "50"});
+        cpus.Add(new List<object>{"Napdragon", "8 Gen 2", "10", "350"});
+        cpus.Add(new List<object>{"Napdragon", "6 Gen 2", "9", "325"});
+        cpus.Add(new List<object>{"Napdragon", "4 Gen 2", "8", "300"});
+        cpus.Add(new List<object>{"Napdragon", "2 Gen 2", "7", "275"});
+
+        cpus.Add(new List<object>{"Napdragon", "8 Gen 1", "9", "325"});
+        cpus.Add(new List<object>{"Napdragon", "6 Gen 1", "8", "300"});
+        cpus.Add(new List<object>{"Napdragon", "4 Gen 1", "7", "275"});
+        cpus.Add(new List<object>{"Napdragon", "2 Gen 1", "6", "250"});
+
+        cpus.Add(new List<object>{"Napdragon", "890", "8", "300"});
+        cpus.Add(new List<object>{"Napdragon", "690", "7", "275"});
+        cpus.Add(new List<object>{"Napdragon", "490", "6", "250"});
+        cpus.Add(new List<object>{"Napdragon", "290", "5", "225"});
+
+        cpus.Add(new List<object>{"Napdragon", "880", "7", "200"});
+        cpus.Add(new List<object>{"Napdragon", "680", "6", "175"});
+        cpus.Add(new List<object>{"Napdragon", "480", "5", "150"});
+        cpus.Add(new List<object>{"Napdragon", "280", "4", "125"});
+
+        cpus.Add(new List<object>{"Napdragon", "870", "6", "175"});
+        cpus.Add(new List<object>{"Napdragon", "670", "5", "150"});
+        cpus.Add(new List<object>{"Napdragon", "470", "4", "125"});
+        cpus.Add(new List<object>{"Napdragon", "270", "3", "100"});
+
+        cpus.Add(new List<object>{"Napdragon", "860", "5", "150"});
+        cpus.Add(new List<object>{"Napdragon", "660", "4", "125"});
+        cpus.Add(new List<object>{"Napdragon", "460", "3", "100"});
+        cpus.Add(new List<object>{"Napdragon", "260", "2", "75"});
+
+        cpus.Add(new List<object>{"Napdragon", "850", "4", "125"});
+        cpus.Add(new List<object>{"Napdragon", "650", "3", "100"});
+        cpus.Add(new List<object>{"Napdragon", "450", "2", "75"});
+        cpus.Add(new List<object>{"Napdragon", "250", "1", "50"});
+
+        cpus.Add(new List<object>{"Napdragon", "840", "3", "100"});
+        cpus.Add(new List<object>{"Napdragon", "640", "2", "75"});
+        cpus.Add(new List<object>{"Napdragon", "440", "1", "50"});
+        cpus.Add(new List<object>{"Napdragon", "240", "0", "25"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 9900", "10", "240"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 9700", "9", "220"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 9500", "8", "200"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 8900", "9", "220"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 8700", "8", "200"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 8500", "7", "180"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 7900", "8", "200"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 7700", "7", "180"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 7500", "6", "160"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 6900", "7", "180"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 6700", "6", "160"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 6500", "5", "140"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 5900", "6", "160"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 5700", "5", "140"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 5500", "4", "120"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 4900", "5", "140"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 4700", "4", "120"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 4500", "3", "100"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 3900", "4", "120"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 3700", "3", "100"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 3500", "2", "80"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 2900", "3", "100"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 2700", "2", "80"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 2500", "1", "60"});
+
+        cpus.Add(new List<object>{"MovieTek", "Dimension 1900", "2", "80"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 1700", "1", "60"});
+        cpus.Add(new List<object>{"MovieTek", "Dimension 1500", "0", "40"});
         #endregion
         
         // make epic buttons
