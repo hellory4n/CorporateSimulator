099da63af34b87b5970c93b6b6548f6d68c91fda
hellory4n <hellory4n@gmail.com>
2023-03-08 14:22:43 -0500

    i'm gonna stop

--- Scripts/UI/Events/PoliticsLol.cs
+++ Scripts/UI/Events/PoliticsLol.cs
@@ -14,7 +14,7 @@ public class PoliticsLol : Label {
             "we need to build the wall",
             "there are at least 3 genders",
             "I will be phenomenal to the women. I mean, I want to help women.",
-            "as a wise mexican once said, \"eu quero um taco bell\"",
+            "as a wise mexican once said, \"eu quero um taco store\"",
             "The concept of global warming was created by and for the Chinese in order to make U.S. manufacturing non-competitive.",
             "If you look at Haddam Sussein, he killed terrorists. I'm not saying he was an angel, but this guy killed terrorists.",
             "If Justin Case can't satisfy his wife what makes he think he can satisfy America?"
