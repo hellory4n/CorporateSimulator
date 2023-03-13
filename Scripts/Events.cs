e1a7aa2bc2689f33ad8a60e998949119f6913f37
hellory4n <hellory4n@gmail.com>
2023-03-13 17:36:31 -0500

    dgfhmj

--- Scripts/Events.cs
+++ Scripts/Events.cs
@@ -1138,6 +1138,17 @@ public class Events : Node2D {
             Global.NPTimesY9M4 = true;
         }
         #endregion
+        #region breaking news y9 m6
+        if (Global.Year==9 && Global.Month==6 && Global.Week==3 && !Global.NPTimesY9M6 &&
+        !Global.PausedTime && Global.Industries[0]=="phones") {
+            var yes = (PackedScene)ResourceLoader.Load("res://Scenes/Ball.tscn");
+            Ball OK = (Ball)yes.Instance();
+            OK.Init("news", "News/NPTimes/Y9M6");
+            OK.ZIndex = 100;
+            GetTree().Root.AddChild(OK);
+            Global.NPTimesY9M6 = true;
+        }
+        #endregion
         base._Process(delta);
     }
 }
--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -152,4 +152,5 @@ public class Global : Node2D {
     public static bool NPTimesY8M6 = false;
     public static bool NPTimesY8M12 = false;
     public static bool NPTimesY9M4 = false;
+    public static bool NPTimesY9M6 = false;
 }
