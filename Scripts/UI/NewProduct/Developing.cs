335de95cfbdbe1656e761e9a9befd6789d564456
hellory4n <hellory4n@gmail.com>
2023-03-18 19:29:05 -0500

    huge change

--- Scripts/UI/NewProduct/Developing.cs
+++ Scripts/UI/NewProduct/Developing.cs
@@ -17,6 +17,15 @@ public class Developing : Label {
 
     public override void _Process(float delta) {
         waitTime = 60/Global.TimeSpeed;
+
+        // so you can hire employees in the middle of development and it'll affect something
+        foreach (var idiot in Global.Employees) {
+            if (idiot.Rating > 0)
+                waitTime -= (int)idiot.Rating/2;
+        }
+        if (waitTime < 1)
+            waitTime = 1;
+
         if (Engine.GetIdleFrames() % (ulong)waitTime == 0 && Global.PausedTime == false) {
             Global.DevelopingProgress++;
             Text = "Developing " + Global.DevelopingName + ": " + Global.DevelopingProgress + "%";
