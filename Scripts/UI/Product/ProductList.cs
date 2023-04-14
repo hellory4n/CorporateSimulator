162bdc99b5ccfb32a8f2ccf030c86a4dc727eb7e
hellory4n <hellory4n@gmail.com>
2023-04-14 13:50:37 -0400

    oh wowser

--- Scripts/UI/Product/ProductList.cs
+++ Scripts/UI/Product/ProductList.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 public class ProductList : HBoxContainer {
     public override void _Ready() {
@@ -9,7 +10,9 @@ public class ProductList : HBoxContainer {
         if (Global.Products.Count > 0)
             GetNode<Label>("../../OhShoes").QueueFree();
 
-        foreach (var product in Global.Products) {
+        // we do a reverse for loop so the newest products are shown first
+        for (int i = Global.Products.Count - 1; i >= 0 ; i--) {
+            object product = Global.Products[i];
             ProductItem OK = (ProductItem)yes.Instance();
             GD.Print(product.ToString());
             // cast doesn't work haha yes
