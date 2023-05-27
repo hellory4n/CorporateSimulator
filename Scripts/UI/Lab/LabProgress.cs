6a196fe6d1d102d7bef9166da7b9088b847168f5
hellory4n <hellory4n@gmail.com>
2023-05-27 14:18:26 -0400

    stupidity

--- Scripts/UI/Lab/LabProgress.cs
+++ Scripts/UI/Lab/LabProgress.cs
@@ -2,14 +2,15 @@ using Godot;
 using System;
 
 public class LabProgress : Label {
-    public override void _Process(float delta) {
+    public override void _Ready() {
         if (Global.LabCurrentProject == "nothing") {
             Text = "The lab is currently doing nothing.";
         } else {
-            double coolPercentage = ((Global.LabTotalCost - Global.LabThingsToSpend) / Global.LabTotalCost) * 100;
-            GD.Print(coolPercentage);
+            double totalCost = Global.LabTotalCost;
+            double thingsToSpend = Global.LabThingsToSpend;
+            int coolPercentage = (int)(((totalCost - thingsToSpend) / totalCost) * 100);
             Text = $"Researching {Global.LabCurrentProject}: {coolPercentage}%";
         }
-        base._Process(delta);
+        base._Ready();
     }
 }
