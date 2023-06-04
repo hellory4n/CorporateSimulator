ae2f57b6cc4e4cbb24cf8aa3ea9f2a0b14e3e8e3
hellory4n <hellory4n@gmail.com>
2023-06-04 13:58:33 -0400

    sabotage

---
+++ Scripts/UI/Government/USA/UsaSabotagePay.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class UsaSabotagePay : TextureButton {
+    public override void _Ready() {
+        this.Connect("pressed", this, nameof(Click));
+    }
+
+    public override void _Process(float delta) {
+        Slider coolSlider = GetNode<Slider>("../Slider1");
+        GetNode<Label>("../Slider1/Label").Text = "$"+String.Format("{0:n0}", coolSlider.Value);
+        base._Process(delta);
+    }
+
+    public void Click() {
+        Global.Money -= (long)GetNode<Slider>("../Slider1").Value;
+        Global.UnitedStates.Friendship -= (int)GetNode<Slider>("../Slider1").Value/100000000;
+        GetParent().QueueFree();
+    }
+}
