51b70a202b875e8fb6ed2257e9f146a8b0b9f1b3
hellory4n <hellory4n@gmail.com>
2023-06-04 12:26:50 -0400

    a text that changes its color omgomgomgomg

--- Scripts/Save/GovernmentStuff.cs
+++ Scripts/Save/GovernmentStuff.cs
@@ -1,43 +1,43 @@
 public class CountryDiplomacy {
-    int Friendship = 50;
-    bool TradeAgreement = false;
-    bool BeingSpied = false;
+    public int Friendship = 50;
+    public bool TradeAgreement = false;
+    public bool BeingSpied = false;
 }
 
 public class ArmyStuff {
-    int Soldiers = 0;
-    int Ammo = 0;
-    int InfantryWeapons = 0;
-    int ArmoredCars = 0;
-    int CombatVehicles = 0;
-    int Artillery = 0;
-    int RPG = 0;
-    int BallisticMissiles = 0;
-    int Tanks = 0;
-    int MassDestruction = 0;
+    public int Soldiers = 0;
+    public int Ammo = 0;
+    public int InfantryWeapons = 0;
+    public int ArmoredCars = 0;
+    public int CombatVehicles = 0;
+    public int Artillery = 0;
+    public int RPG = 0;
+    public int BallisticMissiles = 0;
+    public int Tanks = 0;
+    public int MassDestruction = 0;
 }
 
 public class NavyStuff {
-    int Sailors = 0;
-    int Ammo = 0;
-    int PatrolBoats = 0;
-    int Amphibious = 0;
-    int BallisticMissiles = 0;
-    int Submarines = 0;
-    int AircraftCarrier = 0;
-    int Warship = 0;
-    int Destroyer = 0;
-    int MassDestruction = 0;
+    public int Sailors = 0;
+    public int Ammo = 0;
+    public int PatrolBoats = 0;
+    public int Amphibious = 0;
+    public int BallisticMissiles = 0;
+    public int Submarines = 0;
+    public int AircraftCarrier = 0;
+    public int Warship = 0;
+    public int Destroyer = 0;
+    public int MassDestruction = 0;
 }
 
 public class AirForceStuff {
-    int Pilots = 0;
-    int Ammo = 0;
-    int Surveillance = 0;
-    int Reconnaissance = 0;
-    int Helicopters = 0;
-    int AttackAircraft = 0;
-    int FighterPlanes = 0;
-    int Bombers = 0;
-    int MassDestruction = 0;
+    public int Pilots = 0;
+    public int Ammo = 0;
+    public int Surveillance = 0;
+    public int Reconnaissance = 0;
+    public int Helicopters = 0;
+    public int AttackAircraft = 0;
+    public int FighterPlanes = 0;
+    public int Bombers = 0;
+    public int MassDestruction = 0;
 }
---
+++ Scripts/UI/Government/USA/UsaFriendship.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class UsaFriendship : Label {
+    public override void _Process(float delta) {
+        Text = "Diplomatic Friendship: " + Global.UnitedStates.Friendship + "%";
+        // ally
+        if (Global.UnitedStates.Friendship > 60) {
+            this.AddColorOverride("font_color", new Color(0.242f, 0.726f, 0.285f, 1));
+        // enemy
+        } else if (Global.UnitedStates.Friendship < 40) {
+            this.AddColorOverride("font_color", new Color(1f, 0.281f, 0.394f, 1));
+        // doesn't care about you
+        } else {
+            this.AddColorOverride("font_color", new Color(0f, 0f, 0f, 1f));
+        }
+        base._Process(delta);
+    }
+}
